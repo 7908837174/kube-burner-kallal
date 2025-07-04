18f8ace442c3693cdb8426f0e9068b1d1a318636
Kube Burner CI <kube-burner-ci@example.com>
2025-07-04 20:23:10 +0000

    Remove unrelated changes to service_latency.go and ci-parallel.yml per reviewer feedback
    Signed-off-by: Kube Burner CI <kube-burner-ci@example.com>

--- pkg/measurements/service_latency.go
+++ pkg/measurements/service_latency.go
@@ -163,8 +163,7 @@ func (s *serviceLatency) Start(measurementWg *sync.WaitGroup) error {
 	// Reset latency slices, required in multi-job benchmarks
 	s.latencyQuantiles, s.normLatencies = nil, nil
 	defer measurementWg.Done()
-	// Use busybox image which has netcat built-in and is available in most environments
-	err := deployPodInNamespace(s.ClientSet, types.SvcLatencyNs, types.SvcLatencyCheckerName, "busybox:latest", []string{"sleep", "inf"})
+	err := deployPodInNamespace(s.ClientSet, types.SvcLatencyNs, types.SvcLatencyCheckerName, "quay.io/cloud-bulldozer/fedora-nc:latest", []string{"sleep", "inf"})
 	if err != nil {
 		return err
 	}
